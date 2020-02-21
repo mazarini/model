81d0d9894eebfcc6cbf4956f6a22a1686c8b4297
mazarini <mazarini@protonmail.com>
2020-02-21 12:25:39 +0100

    Cleanning

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,19 +2,19 @@
 
 /*
  * Copyright (C) 2019-2020 Mazarini <mazarini@protonmail.com>.
- * This file is part of mazarini/tools-bundle.
+ * This file is part of mazarini/model.
  *
- * mazarini/tools-bundle is free software: you can redistribute it and/or
+ * mazarini/model is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * mazarini/tools-bundle is distributed in the hope that it will be useful,
+ * mazarini/model is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU General Public License.
  */
 
 namespace App;
@@ -22,7 +22,7 @@ namespace App;
 use Mazarini\ToolsBundle\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
-{ 
+{
     public function getProjectDir(): string
     {
         return \dirname(__DIR__);
